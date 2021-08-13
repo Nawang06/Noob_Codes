cee6c1a304b5a7befa41cdf61a3e40bbed60b890
Nawang06 <nawangla09@gmail.com>
2021-08-13 18:42:18 +0530

    Removed a file named quiz_game

--- quiz_game/question_model.py
+++
@@ -1,9 +0,0 @@
-class Question:
-
-    def __init__(self, q_text, q_answer, q_level, q_category):
-        self.question = q_text
-        self.answer = q_answer
-        self.difficulty = q_level
-        self.category = q_category
-
-
